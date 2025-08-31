613d36deccf868c89c63cbe5fe2ec4ec5fe83c56
joseoliv <jose@ufscar.br>
2025-08-31 15:46:23 -0300

    upload ddm to repository

---
+++ ex_flutter_riverpod/android/app/src/main/kotlin/com/example/ddm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ddm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ex_flutter_riverpod/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+/// a provider for int _counter = 0;
+final counterProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerStatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
+  void _incrementCounter() {
+    /// increment counter
+    ref.read(counterProvider.notifier).state++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer(
+              builder: (context, ref, child) {
+                final counter = ref.watch(counterProvider);
+                return Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            Consumer(
+              builder: (context, ref, child) {
+                final counter = ref.read(counterProvider);
+                //final counter = ref.watch(counterProvider);
+                return Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
